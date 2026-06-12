12b9ff016a58aedc0ec6c1774cef5aaabfea8643
BackRob <Ignacio.vallejosca@gmail.com>
2026-06-12 01:59:34 -0400

    Escribiendo SaveTours

--- src/main/LlanquihueTourApp/src/data/SaveTours.java
+++ src/main/LlanquihueTourApp/src/data/SaveTours.java
@@ -1,4 +1,23 @@
 package src.data;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import src.model.Tour;
 
 public class SaveTours {
+    public static void GuardarDatos(Tour tour){
+
+        try(BufferedWriter escribir = new BufferedWriter(
+                new FileWriter("\"src/main/LlanquihueTourApp/src/resources/tours.txt\"")) ){
+
+
+        }catch (FileNotFoundException e){
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
