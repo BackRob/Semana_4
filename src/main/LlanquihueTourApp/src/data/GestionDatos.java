f670c5a8a924eb94a8e187a6a4ad84ca351f2860
BackRob <Ignacio.vallejosca@gmail.com>
2026-06-22 20:04:00 -0400

    Creacion de metodos Gestion Datos

--- src/main/LlanquihueTourApp/src/data/GestionDatos.java
+++ src/main/LlanquihueTourApp/src/data/GestionDatos.java
@@ -6,20 +6,24 @@ import java.util.ArrayList;
 
 
 public class GestionDatos {
-    private ArrayList<Tour> listaTour = new ArrayList<>();
+    private final ArrayList<Tour> listaTour = new ArrayList<>();
 
-
+    //constructor con datos cargados
+    public GestionDatos(){
+        cargarTours();
+    }
 
 
     //metodo para cargar datos al arraylist
     public void cargarTours(){
+        listaTour.clear();
         StringBuilder lineasError = new StringBuilder("\n----Lineas Con Error----\n");
         int contadorBuenas = 0;
         int contadorlineas = 0;
         int contadorError = 0;
         try(BufferedReader leer = new BufferedReader(new FileReader("src/main/LlanquihueTourApp/src/resources/tours.txt"))){   //abre el archivo y arroja error si no encuentra
             String[] listAux; //lista para guardar split
-            String linea; //variable para consumir txt
+            String linea; //variable auxiliar para consumir txt
 
             while((linea = leer.readLine())!=null){ //si la linea existe la lee y consume
                 contadorlineas++; //cuenta las lineas
@@ -97,28 +101,123 @@ public class GestionDatos {
         }
 
         System.out.println(lineasError); //muetra por consola todos los datos en error y el status.
-        return listaTour;
     }
 
 
 
     //Metodo para guardar datos
-    public static void guardarDatos(Tour tour){
+    public void guardarDatos(Tour tour){
+        if (tour != null) {
+            try (BufferedWriter escribir = new BufferedWriter(
+                    new FileWriter("src/main/LlanquihueTourApp/src/resources/tours.txt", true))) {
+                escribir.write(tour.getNombre() + "," + tour.getTipo() + "," + tour.getPrecio());
+                escribir.newLine();
+                System.out.println("Archivo guardado con exito");
+                listaTour.add(tour); //asi se evita volver a cargar toda la lista
 
-        try(BufferedWriter escribir = new BufferedWriter(
-                new FileWriter("src/main/LlanquihueTourApp/src/resources/tours.txt",true)) ){
-            escribir.write(tour.getNombre()+","+tour.getTipo()+","+tour.getPrecio());
-            escribir.newLine();
-            System.out.println("Archivo guardado con exito");
+            } catch (FileNotFoundException e) {
+                System.out.println("ERROR: Archivo no encontrado");
 
-        }catch (FileNotFoundException e){
-            System.out.println("ERROR: Archivo no encontrado");
-
-        } catch (IOException e) {
-            System.out.println("ERROR: Errror al esribir en el archivo");
+            } catch (IOException e) {
+                System.out.println("ERROR: Errror al esribir en el archivo");
+            }
+        }else{
+            System.out.println("Error: TOUR null");
         }
-
 
     }
 
+    //Metodo para buscar por nombre y mostrar por consola
+    public void buscarNombre(String nombre){
+        int contadorCoincidencias = 0;
+        StringBuilder sb = new StringBuilder("Nombre Buscado: ").append(nombre).append("\n");
+        sb.append("----COINCIDENCIAS----\n");
+        if (listaTour!=null){
+            for(Tour tour:listaTour){
+                if(tour.getNombre().equalsIgnoreCase(nombre)){
+                    contadorCoincidencias++;
+                    sb.append(contadorCoincidencias).append(".-").append(tour).append("\n");
+                }
+            }
+
+        }else{
+            System.out.println("ERROR: Lista nula");
+            return;
+        }
+
+        if (contadorCoincidencias == 0){
+            sb.setLength(0);
+            sb.append("SIN COINCIDENCIAS PARA NOMBRE: \"").append(nombre).append("\"\n");
+
+        }
+
+        System.out.println(sb);
+    }
+
+    //Metodo para buscar por tipo y mostrar por consola
+    public void buscarTipo(String tipo){
+        int contadorCoincidencias = 0;
+        StringBuilder sb = new StringBuilder("Tipo Buscado: ").append(tipo).append("\n");
+        sb.append("----COINCIDENCIAS----\n");
+        if (listaTour!=null){
+            for(Tour tour:listaTour){
+                if(tour.getTipo().equalsIgnoreCase(tipo)){
+                    contadorCoincidencias++;
+                    sb.append(contadorCoincidencias).append(".-").append(tour).append("\n");
+                }
+            }
+
+        }else{
+            System.out.println("ERROR: Lista nula");
+            return;
+        }
+
+        if (contadorCoincidencias == 0){
+            sb.setLength(0);
+            sb.append("SIN COINCIDENCIAS PARA TIPO:  \"").append(tipo).append("\"\n");
+
+        }
+
+        System.out.println(sb);
+    }
+
+    //Metodo para filtrar recibiendo el precio y el operador
+    public void filtrarPrecio(double precio,String operador){
+        StringBuilder sb = new StringBuilder("Precio Buscado: ").append(precio).append(" | Operador: ");
+                sb.append(operador).append("\n");
+        sb.append("----Filtrado----\n");
+
+        int contador = 0;
+        for (Tour tour : listaTour) {
+            switch (operador.trim()) {
+
+                case ">":
+                    if (tour.getPrecio() > precio)  sb.append(tour).append("\n");
+                    contador++;
+                    break;
+
+                case ">=":
+                    if (tour.getPrecio() >= precio) sb.append(tour).append("\n");
+                    contador++;
+                    break;
+
+                case "<":
+                    if (tour.getPrecio() < precio) sb.append(tour).append("\n");
+                    contador++;
+                    break;
+
+                case "<=":
+                    if (tour.getPrecio() <= precio) sb.append(tour).append("\n");
+                    contador++;
+                    break;
+            }
+        }
+
+
+
+        System.out.println(sb);
+    }
+
+
 }
+
--- src/main/LlanquihueTourApp/src/ui/Main.java
+++ src/main/LlanquihueTourApp/src/ui/Main.java
@@ -1,10 +1,13 @@
 package src.ui;
-
+import src.data.GestionDatos;
 
 
 public class Main {
     public static void main(String[] args) {
-
+        GestionDatos gestor = new GestionDatos();
+        gestor.buscarNombre("lago petrolhue");
+        gestor.buscarTipo("gastronomico");
+        gestor.filtrarPrecio(4000,">");
 
     }
 }
